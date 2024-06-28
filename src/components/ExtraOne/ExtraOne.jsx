28a7192ed582505883b6f5c2451025b102b08ae6
hemel17 <redewanmahmud@gmail.com>
2024-06-29 01:02:53 +0600

    extra one added

---
+++ src/components/ExtraOne/ExtraOne.jsx
@@ -0,0 +1,31 @@
+import { Typography } from "@material-tailwind/react";
+
+const ExtraOne = () => {
+  return (
+    <section className="my-4 md:my-6">
+      <Typography variant="h2" className="text-center font-museo">
+        Why you should own a painting of forest
+      </Typography>
+      \
+      <Typography className="relative w-4/5 mx-auto text-lg text-justify md:w-3/5">
+        Having a painting of a forest landscape can offer numerous benefits,
+        both aesthetic and emotional. Forest landscapes often feature vibrant
+        colors, intricate details, and a sense of depth that can enhance the
+        visual appeal of any room, creating a focal point that draws the eye and
+        sparks conversation. Beyond their beauty, these paintings can have a
+        calming effect, helping to reduce stress and promote relaxation by
+        evoking the tranquility of nature. For those who live in urban areas or
+        don&apos;t have easy access to natural settings, a forest landscape
+        painting can provide a much-needed visual connection to the natural
+        world, fostering a sense of peace and well-being. Additionally, the
+        beauty of nature often inspires creativity and wonder, making such
+        paintings a source of inspiration that can stimulate the imagination.
+        Forests also carry rich symbolic meanings, representing growth, mystery,
+        and renewal, which can add a deeper layer of significance to the artwork
+        in your home.
+      </Typography>
+    </section>
+  );
+};
+
+export default ExtraOne;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import CraftItems from "../CraftItems/CraftItems";
+import ExtraOne from "../ExtraOne/ExtraOne";
 import Slider from "../Slider/Slider";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <Slider />
       <CraftItems />
+      <ExtraOne />
     </>
   );
 };
