ac482e89bb603d16734aae249c69c6ec6b9a7983
hemel17 <redewanmahmud@gmail.com>
2024-07-04 01:04:44 +0600

    card added

--- src/components/ArtAndCraftCategories/ArtAndCraftCategories.jsx
+++ src/components/ArtAndCraftCategories/ArtAndCraftCategories.jsx
@@ -1,13 +1,58 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+} from "@material-tailwind/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const ArtAndCraftCategories = () => {
+  const arts = [
+    {
+      name: "Forest Based Landscape Painting",
+      photo: "https://i.ibb.co/KmWBYpG/10329.jpg",
+      url: "/forest",
+    },
+    {
+      name: "Floral Watercolor Art",
+      photo:
+        "https://i.ibb.co/58zjxcL/watercolor-painting-with-…ti-colored-abstract-backgrounds-generative-ai.jpg",
+      url: "/floral",
+    },
+    {
+      name: "Abstract Oil Painting",
+      photo: "https://i.ibb.co/WyQWdb1/close-up-oil-painting-texture.jpg",
+      url: "/abstract",
+    },
+    {
+      name: "Architectural Charcoal Drawing",
+      photo: "https://i.ibb.co/smqq3XC/architecture.jpg",
+      url: "/architecture",
+    },
+    {
+      name: "Mountain View Canvas",
+      photo:
+        "https://i.ibb.co/2cK2JDL/mountain-landscape-outdoors-snow-scene-generative-ai.jpg",
+      url: "/mountain",
+    },
+    {
+      name: "Realistic Oil Potrait",
+      photo:
+        "https://i.ibb.co/qn2S75h/young-adult-female-smiling-closeup-portrait-generative-ai.jpg",
+      url: "/realistic",
+    },
+  ];
+
   const [forest, setForest] = useState([]);
   const [abstract, setAbstract] = useState([]);
   const [architecture, setArchitecture] = useState([]);
   const [floral, setFloral] = useState([]);
   const [mountain, setMountain] = useState([]);
   const [realistic, setRealistic] = useState([]);
+
+  console.log(forest, abstract, architecture, floral, mountain, realistic);
 
   useEffect(() => {
     const loadData = async () => {
@@ -42,6 +87,37 @@ const ArtAndCraftCategories = () => {
       abstract : {floral.length}
       abstract : {mountain.length}
       abstract : {realistic.length}
+      <section className="my-4 md:my-10">
+        <Typography variant="h2" className="text-center font-museo">
+          Art & Craft Categories
+        </Typography>
+        <div className="grid grid-cols-1 gap-6 my-4 md:my-6 md:grid-cols-2 lg:grid-cols-3">
+          {arts.map((art, idx) => {
+            return (
+              <Link key={idx} to={art.url}>
+                <Card className="mx-auto mt-6 w-96">
+                  <CardHeader color="blue-gray" className="relative h-56">
+                    <img
+                      src={art.photo}
+                      alt={art.name}
+                      className="w-full h-full"
+                    />
+                  </CardHeader>
+                  <CardBody>
+                    <Typography
+                      variant="h5"
+                      color="blue-gray"
+                      className="text-center"
+                    >
+                      {art.name}
+                    </Typography>
+                  </CardBody>
+                </Card>
+              </Link>
+            );
+          })}
+        </div>
+      </section>
     </section>
   );
 };
