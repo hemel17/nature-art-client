74aec4620b40b436d347f6e8174efcbeb83261ae
hemel17 <redewanmahmud@gmail.com>
2024-07-04 00:07:22 +0600

    data loaded

--- src/components/ArtAndCraftCategories/ArtAndCraftCategories.jsx
+++ src/components/ArtAndCraftCategories/ArtAndCraftCategories.jsx
@@ -4,6 +4,10 @@ import { useEffect, useState } from "react";
 const ArtAndCraftCategories = () => {
   const [forest, setForest] = useState([]);
   const [abstract, setAbstract] = useState([]);
+  const [architecture, setArchitecture] = useState([]);
+  const [floral, setFloral] = useState([]);
+  const [mountain, setMountain] = useState([]);
+  const [realistic, setRealistic] = useState([]);
 
   useEffect(() => {
     const loadData = async () => {
@@ -11,12 +15,22 @@ const ArtAndCraftCategories = () => {
       const forestData = await axios.get("http://localhost:5000/forest");
       setForest(forestData.data);
       // abstract
-      const abstractData = await axios.get("http://localhost:5000/forest");
+      const abstractData = await axios.get("http://localhost:5000/abstract");
       setAbstract(abstractData.data);
       // architecture
+      const architectureData = await axios.get(
+        "http://localhost:5000/architecture"
+      );
+      setArchitecture(architectureData.data);
       // floral
+      const floralData = await axios.get("http://localhost:5000/floral");
+      setFloral(floralData.data);
       // mountain
+      const mountainData = await axios.get("http://localhost:5000/mountain");
+      setMountain(mountainData.data);
       // realistic
+      const realisticData = await axios.get("http://localhost:5000/realistic");
+      setRealistic(realisticData.data);
     };
     loadData();
   }, []);
@@ -24,6 +38,10 @@ const ArtAndCraftCategories = () => {
     <section>
       forest : {forest.length}
       abstract : {abstract.length}
+      abstract : {architecture.length}
+      abstract : {floral.length}
+      abstract : {mountain.length}
+      abstract : {realistic.length}
     </section>
   );
 };
